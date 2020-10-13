a02674494aebc5542ce157360dc98a5780aca03a
Mohit Tikoo <42917297+tikoomohit@users.noreply.github.com>
2020-10-13 13:20:46 +0530

    Update Footer.js

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -20,17 +20,7 @@ const Footer = (props) => {
           <ul className="social-links">{renderNetworks()}</ul>
 
           <ul>
-            <li>Modified by Mohit</li>
-          </ul>
-
-          <ul className="copyright">
-            <li>&copy; TM</li>
-            <li>
-              Design by{" "}
-              <a title="Styleshout" href="http://www.styleshout.com/">
-                Styleshout
-              </a>
-            </li>
+            <li>Designed by Mohit</li>
           </ul>
         </div>
         <div id="go-top">
